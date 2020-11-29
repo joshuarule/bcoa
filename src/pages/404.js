12dfd04d0f8b108cba2c6bd8cbc9ea8a5439b00c
joshuarule <joshua@joshuarule.com>
2020-11-29 18:21:30 -0500

    fixes 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,13 +3,15 @@ import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
 const NotFoundPage = (props) => (
-  <Layout {...props} >
+  <Layout {...props} render={() => 
+    <>
     <SEO />
     <div>
       <h1>NOT FOUND</h1>
       <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
     </div>
-  </Layout>
+    </>
+  }/>
 )
 
 export default NotFoundPage
