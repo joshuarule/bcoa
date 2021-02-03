f4310e09fb8c9bacfc87666bb1e07c0200dafdb2
Joshua Rule Dobson <joshua@joshuarule.com>
2021-02-03 08:53:58 -0500

    header fix test

--- src/components/HeaderNav.js
+++ src/components/HeaderNav.js
@@ -1,54 +1,87 @@
-import React from 'react';
+import React from "react";
 import Link from "gatsby-link";
 import { icons } from "./Icons";
-import useSiteMetadata from './SiteMetadata';
+import useSiteMetadata from "./SiteMetadata";
 
 const Menu = ({ visible, toggleMenu, navHeight }) => {
-  const { menuBackground } = useSiteMetadata()
-  
+  const { menuBackground } = useSiteMetadata();
+
   return (
-    <nav  
-      className={`menu f-navigation ${visible ? 'visible' : ''}`}
-      style={{ backgroundImage: `url("${menuBackground.childImageSharp.fixed.src}")`, height: `${navHeight}px` }}
+    <nav
+      className={`menu f-navigation ${visible ? "visible" : ""}`}
+      style={{
+        backgroundImage: `url("${menuBackground.childImageSharp.fixed.src}")`,
+        height: `${navHeight}px`,
+      }}
     >
       <ul className="c-white">
         <li>
-          <Link to="/#featured" onClick={toggleMenu}>Featured</Link>
+          <Link to="/#featured" onClick={toggleMenu}>
+            Featured
+          </Link>
         </li>
         <li>
-          <Link to="/work" onClick={toggleMenu}>Index</Link>
+          <Link to="/work" onClick={toggleMenu}>
+            Index
+          </Link>
         </li>
         <li>
-          <Link to="/news" onClick={toggleMenu}>News</Link>
+          <Link to="/news" onClick={toggleMenu}>
+            News
+          </Link>
         </li>
         <li>
-          <Link to="/about" onClick={toggleMenu}>About</Link>
+          <Link to="/about" onClick={toggleMenu}>
+            About
+          </Link>
         </li>
         <li>
-          <Link to="/contact" onClick={toggleMenu}>Contact</Link>
+          <Link to="/contact" onClick={toggleMenu}>
+            Contact
+          </Link>
         </li>
       </ul>
     </nav>
-  )
+  );
 };
 
 const headerLogoClick = (visible, toggleMenu) => {
-  if (visible) { toggleMenu() };
-}
+  if (visible) {
+    toggleMenu();
+  }
+};
 
-export default ({ visible, toggleMenu, isWindowLarge, navHeight, handleMenuButtonClick }) => {
-  const headerLogo = (isWindowLarge === undefined) ? icons.menuLogoLarge : isWindowLarge ? icons.menuLogoLarge : icons.menuLogoSmall
+export default ({
+  visible,
+  toggleMenu,
+  isWindowLarge,
+  navHeight,
+  handleMenuButtonClick,
+}) => {
+  console.log(isWindowLarge);
+  const headerLogo =
+    isWindowLarge === undefined || isWindowLarge
+      ? icons.menuLogoLarge
+      : icons.menuLogoSmall;
   return (
     <div>
-      <header className={visible ? 'c-white bg-transparent' : ''}>
+      <header className={visible ? "c-white bg-transparent" : ""}>
         <div className="container">
           <div className="flex-1">
-            <Link className="headerLogo" onClick={ () => headerLogoClick(visible, toggleMenu) } to="/">
+            <Link
+              className="headerLogo"
+              onClick={() => headerLogoClick(visible, toggleMenu)}
+              to="/"
+            >
               <span className="screen-reader-text">Home</span>
               {headerLogo}
             </Link>
           </div>
-          <button title="toggle menue" className={`header-menuButton ${visible ? 'isActive' : ''}`} onClick={toggleMenu} >
+          <button
+            title="toggle menue"
+            className={`header-menuButton ${visible ? "isActive" : ""}`}
+            onClick={toggleMenu}
+          >
             <div className="line lineExterior"></div>
             <div className="line lineInterior lineInterior-1"></div>
             <div className="line lineInterior lineInterior-2"></div>
@@ -56,12 +89,12 @@ export default ({ visible, toggleMenu, isWindowLarge, navHeight, handleMenuButto
           </button>
         </div>
       </header>
-      <Menu 
-        visible={visible} 
-        toggleMenu={toggleMenu} 
-        navHeight={navHeight} 
-        handleMenuButtonClick={handleMenuButtonClick} 
+      <Menu
+        visible={visible}
+        toggleMenu={toggleMenu}
+        navHeight={navHeight}
+        handleMenuButtonClick={handleMenuButtonClick}
       />
     </div>
-  )
-}
+  );
+};
