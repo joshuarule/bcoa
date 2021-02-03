024d9bb624ed8eb7eaa6922ea05be960734970ca
Joshua Rule Dobson <joshua@joshuarule.com>
2021-02-03 10:30:43 -0500

    removes work description

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,115 +1,133 @@
-import React, {Component} from "react";
-import {graphql} from 'gatsby';
+import React, { Component } from "react";
+import { graphql } from "gatsby";
 import Link from "gatsby-link";
-import Img from 'gatsby-image';
-import ProjectFilter from '../components/ProjectFilter';
+import Img from "gatsby-image";
+import ProjectFilter from "../components/ProjectFilter";
 import SEO from "../components/SEO";
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
 
 class Work extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      filterValue: 'all'
-    }
+      filterValue: "all",
+    };
   }
 
   filterProjects(project) {
-    if(this.state.filterValue === 'all') {return true;}
+    if (this.state.filterValue === "all") {
+      return true;
+    }
     return project.frontmatter.type === this.state.filterValue;
   }
 
   renderFilterTransition = (filterValue) => {
-    this.setState({inTransition: true});
+    this.setState({ inTransition: true });
     setTimeout(() => this.setState({ filterValue: filterValue }), 250);
     setTimeout(() => this.setState({ inTransition: false }), 500);
-  }
+  };
 
   render() {
     const page = this.props.data.page.frontmatter;
     const projects = this.props.data.projects.nodes;
     const projectOrder = page.projects;
     //creates user defined order of projects from page frontmatter
-    const orderedProjects = projectOrder.map((title) => {
-      return projects.find(project => {
-        return project.frontmatter.title === title.project
+    const orderedProjects = projectOrder
+      .map((title) => {
+        return projects.find((project) => {
+          return project.frontmatter.title === title.project;
+        });
       })
-    }).filter(project => project.frontmatter.isPublished)
+      .filter((project) => project.frontmatter.isPublished);
 
     return (
-        <div className="container
+      <div
+        className="container
                         marginBottom-11
                         bp-1_marginBottom-3
                         bp-2_marginBottom-9"
-        >
-          <SEO
-            postImage={page.seo.image && page.seo.image.childImageSharp.fluid.src}
-            postData={page}
-          />
-          <div className="flex
+      >
+        <SEO
+          postImage={page.seo.image && page.seo.image.childImageSharp.fluid.src}
+          postData={page}
+        />
+        <div
+          className="flex
                           justifySpaceBetween
                           marginTop-7 marginBottom-6
                           bp-1_marginTop-10
                           bp-2_marginTop-17 bp-2_marginBottom-12"
-          >
-            <h1 className="f-page-title">
-              { page.title }
-            </h1>
-            <ProjectFilter isWindowLarge={ this.props.breakpoint === 'large' } onChange={(val) => this.renderFilterTransition(val)}/>
-          </div>
-          <ul className={`${this.state.inTransition ? 'inTransition' : '' } bp-1_grid-3col bp-2_grid-4col`}>
-            { projects &&
-              orderedProjects.filter(project => this.filterProjects(project)).map((project, i) => {
+        >
+          <h1 className="f-page-title">{page.title}</h1>
+          <ProjectFilter
+            isWindowLarge={this.props.breakpoint === "large"}
+            onChange={(val) => this.renderFilterTransition(val)}
+          />
+        </div>
+        <ul
+          className={`${
+            this.state.inTransition ? "inTransition" : ""
+          } bp-1_grid-3col bp-2_grid-4col`}
+        >
+          {projects &&
+            orderedProjects
+              .filter((project) => this.filterProjects(project))
+              .map((project, i) => {
                 return (
                   <li key={i}>
                     <article className="workProject">
-                      <Link to={ `/projects${project.fields.slug}` }>
-                        {project.frontmatter.previewImage.image && 
+                      <Link to={`/projects${project.fields.slug}`}>
+                        {project.frontmatter.previewImage.image && (
                           <Img
-                            fluid={ project.frontmatter.previewImage.image.childImageSharp.fluid }
+                            fluid={
+                              project.frontmatter.previewImage.image
+                                .childImageSharp.fluid
+                            }
                             className="marginBottom-3"
                             alt={project.frontmatter.previewImage.alt}
                           />
-                        }
-                        <h1 className="f-subhead">{ project.frontmatter.title }</h1>
-                        <h2 className=" f-subhead
+                        )}
+                        <h1 className="f-subhead">
+                          {project.frontmatter.title}
+                        </h1>
+                        <h2
+                          className=" f-subhead
                                         marginBottom-9
                                         bp-2_marginBottom-21"
-                        >
-                        </h2>
+                        ></h2>
                       </Link>
                     </article>
                   </li>
-                )
-              })
-            }
-          </ul>
-        </div>
+                );
+              })}
+        </ul>
+      </div>
     );
   }
-    
-};
+}
 
 const WorkPage = (props) => {
   return (
-    <Layout 
-      {...props} 
-      render={(ref, breakpoint) => <Work data={props.data} intersectionRef={ref} breakpoint={breakpoint} /> } 
+    <Layout
+      {...props}
+      render={(ref, breakpoint) => (
+        <Work data={props.data} intersectionRef={ref} breakpoint={breakpoint} />
+      )}
     />
-  )
-}
+  );
+};
 
-export default WorkPage
+export default WorkPage;
 
 export const query = graphql`
   query WorkQuery {
     projects: allMdx(
       filter: {
-        frontmatter: { 
-          templateKey: { regex: "/project/" },
-          isPublished: {eq: true}
-        } 
+        frontmatter: {
+          templateKey: { regex: "/project/" }
+          isPublished: { eq: true }
+        }
       }
     ) {
       nodes {
@@ -128,7 +146,6 @@ export const query = graphql`
               }
             }
           }
-          workDescription
           type
           previewImage {
             image {
@@ -146,7 +163,7 @@ export const query = graphql`
         }
       }
     }
-    page: mdx(fields: {slug: {eq: "/work/"}}) {
+    page: mdx(fields: { slug: { eq: "/work/" } }) {
       id
       frontmatter {
         title
